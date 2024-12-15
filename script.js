54b0fddf8e7a68d6471d8522fa965b40dd376da1
Timur <abkeevtimur@gmail.com>
2024-12-15 17:22:07 +0300

    remake keywords

--- script.js
+++ script.js
@@ -1,6 +1,47 @@
 import {data} from "./data.js";
-console.log(data[0].symbol);
 
+let data2 = []
+let input = document.querySelector(".input")
+let form = document.querySelector(".form")
+form.addEventListener('input', (event) => {                         //поиск 
+    event.preventDefault()
+    section.innerHTML =  ''
+     for(let n = 0; n <= data.length; n++ ){
+        if(data[n].title.toLowerCase().includes(input.value.toLowerCase())){                       //совпадает ли с названием
+            data2[0] = data[n]
+            let article = document.createElement('article')
+            article.classList.add('article')
+            article.innerHTML = `
+                <div class="article__div">
+                    <p class="article__div_img" style='font-size: 48px'>${data2[0].symbol}</p>
+                </div>
+                <div class="article__info">
+                    <p class="article__info_code">${data2[0].title}</p>
+                    <p class="article__info_text">${data2[0].keywords}</p>
+                </div>
+            `
+            section.appendChild(article)
+        } else {                                                                                 //совпадает ли с ключ. словами
+            if(data[n].keywords.toLowerCase().includes(input.value.toLowerCase())){
+                data2[0] = data[n]
+                let article = document.createElement('article')
+                article.classList.add('article')
+                article.innerHTML = `
+                    <div class="article__div">
+                        <p class="article__div_img" style='font-size: 48px'>${data2[0].symbol}</p>
+                    </div>
+                    <div class="article__info">
+                        <p class="article__info_code">${data2[0].title}</p>
+                        <p class="article__info_text">${data2[0].keywords}</p>
+                    </div>
+                `
+                section.appendChild(article)
+            } else{
+
+            }
+        }
+    }
+})
 
 let section = document.querySelector('.main__articles')
 
@@ -9,21 +50,18 @@ let getNumberAtConsole = (el) => {
     console.log(cardNumber);
 }
 
-let remake = ''
+
+let makeDes = (info) => {                                          //изменение ключевых слов
+    data[info].keywords = data[info].keywords.split(' ')
+    data[info].keywords = [... new Set(data[info].keywords)]
+    data[info].keywords = data[info].keywords.join(' ')
+    return data[info].keywords;
+}
 
 
-let addArticle = (info) => {
-    // console.log(info);
-    remake = data[info].keywords.split(' ')
-    for(let del = 0; del <= remake.length-1; del++){
-        // console.log(remake[del]);
-        if(remake[del] === remake[del + 1]){
-            delete remake[del + 1]
-        } else {
-            
-        } 
-    }
-    remake = remake.join(' ')
+
+
+let addArticle = (info) => {                                       //база загрузки карточек
 
     let article = document.createElement('article')
     article.classList.add('article')
@@ -37,19 +75,55 @@ let addArticle = (info) => {
             <p class="article__div_img" style='font-size: 48px'>${data[info].symbol}</p>
         </div>
         <div class="article__info">
-            <p class="article__info_code">${info}</p>
-            <p class="article__info_text">${remake}</p>
+            <p class="article__info_code">${data[info].title}</p>
+            <p class="article__info_text">${data[info].keywords}</p>
         </div>
     `
 
     section.appendChild(article)
-    remake = ''
 }
 
-for(let i = 7; i <= data.length; i++ ){
+for(let i = 0; i <= data.length-1; i++){  // ключ для изменения ключевых слов
+    makeDes(i)
+}
+
+for(let i = 0; i <= data.length; i++ ){  //ключ для базовой загрузки карточек
     addArticle(i)
 }
 
 
+// let makeArticles = (info) => {
+//     let article = document.createElement('article')
+//     article.classList.add('article')
+
+//     article.addEventListener('click', () => {
+//         getNumberAtConsole(article)
+//     })
+
+//     article.innerHTML = `
+//         <div class="article__div">
+//             <p class="article__div_img" style='font-size: 48px'>${data[info].symbol}</p>
+//         </div>
+//         <div class="article__info">
+//             <p class="article__info_code">${data[info].title}</p>
+//             <p class="article__info_text">${data[info].keywords}</p>
+//         </div>
+//     `
+
+//     section.appendChild(article)
+// }
+
+// for(let i = 0; i <= data.length; i++){
+//     makeArticles(i)
+// }
+
+
+
+let arr = ['1', '2', '3', '1', '1', '1', '2']
+let test = () => {
+    let uniqArr = [... new Set(arr)]
+    console.log(uniqArr);
+}
+test()
 
 
